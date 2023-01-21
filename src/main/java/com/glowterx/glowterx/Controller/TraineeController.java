77232790910ac2f8a998d6d81cbc6b437e022fc4
afikahhayazi <afikahfikah01@gmail.com>
2023-01-21 17:53:25 +0800

    hhh

--- src/main/java/com/glowterx/glowterx/Controller/TraineeController.java
+++ src/main/java/com/glowterx/glowterx/Controller/TraineeController.java
@@ -59,4 +59,39 @@ public class TraineeController {
 
         return "fitnesslogin";
     }
+    
+    public Membership validate(String username, String password) {
+        Membership membership= null;
+
+        try (Connection connection = dataSource.getConnection();
+                PreparedStatement statement = connection
+                        .prepareStatement("SELECT * FROM membership WHERE username = ? AND password = ?")) {
+
+            statement.setString(1, username);
+            statement.setString(2, password);
+
+            ResultSet rs = statement.executeQuery();
+
+            if (rs.next()) {
+                admin = new Admin();
+                admin.setId(rs.getInt("id"));
+                admin.setAdminUsername(rs.getString("username"));
+                admin.setAdminPass(rs.getString("password"));
+                admin.setFirstName(rs.getString("firstname"));
+                admin.setLastName(rs.getString("lastname"));
+                admin.setAddress(rs.getString("address"));
+                admin.setCity(rs.getString("city"));
+                admin.setState(rs.getString("state"));
+                admin.setZip(rs.getString("zip"));
+                admin.setPhone(rs.getString("phone"));
+                admin.setEmail(rs.getString("email"));
+                admin.setGender(rs.getString("gender"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return admin;
+    }
+
 }
