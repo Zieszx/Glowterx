a25c28f692bc28bda882c300fbceb0580a7ea29c
afikahhayazi <afikahfikah01@gmail.com>
2023-01-22 16:33:48 +0800

    HHH

--- src/main/java/com/glowterx/glowterx/Controller/TraineeController.java
+++ src/main/java/com/glowterx/glowterx/Controller/TraineeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.glowterx.glowterx.DOA.TraineeDAO;
 import com.glowterx.glowterx.Model.Trainee;
-
+import com.glowterx.glowterx.DOA.MembershipDAO;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
@@ -22,6 +22,8 @@ public class TraineeController {
 
     @Autowired
     private TraineeDAO traineeDAO;
+    @Autowired
+    private TraineeDAO MembershipDAO;
 
     @PostMapping("/registerTrainee")
     public String registerTrainee(@RequestParam("firstname") String firstname,
@@ -61,31 +63,23 @@ public class TraineeController {
         return "fitnesslogin";
     }
     @PostMapping("/membershipPayment")
-    public String registerMembership(@RequestParam("firstname") String firstname,
-            @RequestParam("lastname") String lastname, @RequestParam("username") String username,
-            @RequestParam("password") String password, @RequestParam("phonenum") String phonenum,
-            @RequestParam("email") String email, @RequestParam("gender") String gender,
-            @RequestParam("address") String address, @RequestParam("city") String city,
-            @RequestParam("state") String state, @RequestParam("zip") String zip,
-            @RequestParam("file") MultipartFile file, Model model) {
+    public String registerMembership(@RequestParam("Plan") String Category,
+            @RequestParam("date") String date, @RequestParam("paymentMethod") String paymentMethod,
+             Model model) {
 
-        Trainee trainee = new Trainee();
+            Trainee trainee = (Trainee) session.getAttribute("trainee");
+    
+        /*Trainee trainee = new Trainee();
         trainee.setFirstName(firstname);
         trainee.setLastName(lastname);
         trainee.setTraineeUsername(username);
-        trainee.setTraineePass(password);
-        trainee.setPhone(phonenum);
-        trainee.setEmail(email);
-        trainee.setGender(gender);
-        trainee.setAddress(address);
-        trainee.setCity(city);
-        trainee.setZip(zip);
-        trainee.setState(state);
+        
 
         // save the trainee to the database
-        traineeDAO.insertTrainee(trainee);
+        traineeDAO.insertTrainee(trainee);*/
 
-        try {
+
+        /*try {
             // get the bytes of the image file
             byte[] imageBytes = file.getBytes();
 
@@ -93,7 +87,7 @@ public class TraineeController {
             traineeDAO.uploadProfilePicture(username, imageBytes);
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
 
         return "Trainee/Subscribe";
     }
